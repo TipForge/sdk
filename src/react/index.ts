dc7b76b697b8163facfbc70ddfc956389e193e9e
Bamford <bamfordjnr5@gmail.com>
2026-07-03 19:59:07 +0100

    init: create SDK package structure

--- src/react/index.ts
+++
@@ -1,8 +0,0 @@
-// React hooks layer (optional export)
-// Consumers using React can import from 'tipforge-sdk/react'
-
-export { useTipForge } from './hooks/useTipForge';
-export { useCreator } from './hooks/useCreator';
-export { useTransaction } from './hooks/useTransaction';
-export { useWallet } from './hooks/useWallet';
-export { TipForgeProvider } from './context/TipForgeProvider';
